b8e1fb0c882e8e3c00fa89faf0921cfd6ee78cce
brijeshkumarpatel <brijeshp56@gmail.com>
2016-09-30 17:16:06 +0530

    dhap_test_30/09/2016

---
+++ dhap-test-api/src/main/java/com/thermofisher/healthcheck/HealthCheckApi.java
@@ -0,0 +1,19 @@
+package com.thermofisher.healthcheck;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+/**
+ * Created by patelb on 9/29/2016. This is for checking the health of this application
+ */
+@Path("/healthcheck")
+public interface HealthCheckApi {
+    /**
+     * Checks connectivity to .
+     * void method results in a 204 No Content response in the case of JAX-RS.
+     * so to get response as 200 OK the return type should be boolean.
+     * For success 200 will be sent to caller.
+     */
+    @GET
+    boolean checkHealth();
+}
